15fc5d30d47816c8d881964872af173284887303
David Foote <david.a.foote@gmail.com>
2019-08-23 03:25:17 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -77,6 +77,14 @@ def self.create_table
       new_dog
   end
   
-  def self.find_by_name
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT * FROM dogs WHERE name = ?
+    SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+  end
 end
 
